9bd80819b588738609ca2b196b82a391ce380f74
pk050101 <pk.it050101@gmail.com>
2023-01-07 00:57:15 +0530

    Added Services Contact and About Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
 import Home from './components/Home';
+import Services from './components/Services';
 
 function App() {
   return (
     <div className="App">
       <h1>Home Page</h1>
       <Home/>
+      <Contact/>
+      <Services/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>Contact Us If You Need</h1>
+    </div>
+  )
+}
